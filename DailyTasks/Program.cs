ecd5eea04b3144f8b14edaa4684ad86112b8fcf0
Artimolot <molotokamerigo@gmail.com>
2018-10-18 22:25:52 +0300

    first commit

---
+++ DailyTasks/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTasks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task task = new Task();
+
+			task.completeOperation += Show_Message;
+			task.failOperation += Show_Message;
+
+			Console.ForegroundColor = ConsoleColor.Magenta;
+			Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Дневные задания"));
+			Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+
+			do
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("Список команд:");
+				Console.WriteLine("add - добавить задание");
+				Console.WriteLine("remove - удалить задание");
+				Console.WriteLine("list - показать список заданий");
+				Console.WriteLine();
+
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				Console.Write("Введите желаемую команду: ");
+				string command = Console.ReadLine();
+
+				switch (command)
+				{
+					case "add":
+						Console.WriteLine("Добавление задания");
+						Console.Write("Введите время задания: ");
+						int timeAdd = Convert.ToInt32(Console.ReadLine());
+						Console.Write("Введите название задания: ");
+						string nameTask = Console.ReadLine();
+						task.AddTask(timeAdd, nameTask);
+						Console.WriteLine();
+						break;
+					case "remove":
+						Console.WriteLine("Удаление задания");
+						Console.WriteLine("Введите время задания которое хотите удалить: ");
+						int timeRemove = Convert.ToInt32(Console.ReadLine());
+						task.DeleteTask(timeRemove);
+						Console.WriteLine();
+						break;
+					case "list":
+						Console.WriteLine("Список заданий:");
+						task.ShowListTask();
+						Console.WriteLine();
+						break;
+					default:
+						Console.WriteLine("Такой команды нет");
+						Console.WriteLine();
+						break;
+				}
+			}
+			while (true);
+		}
+
+		private static void Show_Message(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
---
+++ DailyTasks/Task.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTasks
+{
+	public delegate void TaskHandler(string message);
+
+	class Task
+	{
+		public event TaskHandler failOperation;
+		public event TaskHandler completeOperation;
+
+		public Dictionary<int, string> taskList { get; private set; }
+		
+		public Task()
+		{
+			taskList = new Dictionary<int, string>();
+		}
+
+		public void AddTask(int time, string nameTask)
+		{
+			bool inCheck = false;
+			if (taskList.Keys.Count != 0)
+			{
+				foreach (KeyValuePair<int, string> key in taskList)
+				{
+					if (key.Key == time)
+					{
+						inCheck = false;
+						break;
+					}
+					else
+					{
+						inCheck = true;
+					}
+				}
+				if (inCheck == true)
+				{
+					completeOperation($"Задание добавлено на: {time}:00");
+					taskList.Add(time, nameTask);
+				}
+				else
+				{
+					failOperation($"На {time}:00, уже есть задание");
+				}
+			}
+			else
+			{
+				completeOperation($"Задание добавлено на: {time}:00");
+				taskList.Add(time, nameTask);
+			}
+		}
+
+		public void DeleteTask(int time)
+		{
+			bool inCheck = false;
+			if (taskList.Keys.Count != 0)
+			{
+				foreach (KeyValuePair<int, string> key in taskList)
+				{
+					if (key.Key != time)
+					{
+						inCheck = false;
+					}
+					else
+					{
+						inCheck = true;
+						break;
+					}
+				}
+				if(inCheck == true)
+				{
+					completeOperation($"Задание на {time}:00, удалено");
+					taskList.Remove(time);
+				}
+				else
+				{
+					failOperation("Невозможно удалить задание, на такое время нет задания");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Список пуст!");
+			}
+		}
+
+		public void ShowListTask()
+		{
+			if(taskList.Keys.Count != 0)
+			{
+				foreach (KeyValuePair<int, string> list in taskList)
+				{
+					Console.WriteLine($"{list.Key}:00 - {list.Value}");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Пуст!");
+			}
+		}
+	}
+}
